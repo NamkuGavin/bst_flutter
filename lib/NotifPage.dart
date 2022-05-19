15f8c58f506f25c7dba3321585a487be135f1297
ajisuryawanrectmedia <ajisuryawanrectmedia>
2022-05-19 14:15:19 +0700

    feat: [AS] notif page blank

---
+++ lib/NotifPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotifPage extends StatefulWidget {
+  const NotifPage({Key? key}) : super(key: key);
+
+  @override
+  _NotifPageState createState() => _NotifPageState();
+}
+
+class _NotifPageState extends State<NotifPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
