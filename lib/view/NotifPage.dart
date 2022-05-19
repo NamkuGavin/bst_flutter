e14dafaab0a8d320095daaf72081fb0f36598dbd
Aji Suryawan <ajisuryawan@smkrus.sch.id>
2022-05-19 22:23:14 +0700

    fix: [AS] custom slide menu

--- lib/view/NotifPage.dart
+++ lib/view/NotifPage.dart
@@ -1,8 +1,8 @@
-import 'package:bst/AllNotifications.dart';
-import 'package:bst/UnreadNotifications.dart';
+import 'package:bst/view/fragment/AllNotifications.dart';
+import 'package:bst/view/fragment/UnreadNotifications.dart';
 import 'package:flutter/material.dart';
 
-import 'header/HeaderNavigation.dart';
+import '../header/HeaderNavigation.dart';
 
 class NotifPage extends StatefulWidget {
   const NotifPage({Key? key}) : super(key: key);
@@ -12,7 +12,7 @@ class NotifPage extends StatefulWidget {
 }
 
 class _NotifPageState extends State<NotifPage> {
-  final List<Tab> myTabs = <Tab>[
+  final List<Widget> myTabs = <Widget>[
     Tab(text: 'Tampilkan Semua'),
     Tab(text: 'Belum Dibaca'),
   ];
@@ -23,8 +23,13 @@ class _NotifPageState extends State<NotifPage> {
       length: myTabs.length,
       child: Scaffold(
         appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
           title: HeaderNavigation(title: "Notifikasi"),
           bottom: TabBar(
+            indicatorPadding: EdgeInsets.only(left: 20.0, right: 20.0),
+            indicatorColor: Colors.green,
+            labelColor: Colors.black,
             tabs: myTabs,
           ),
         ),
